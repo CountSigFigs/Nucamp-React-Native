3c237b65b60506638ec159491422e16ae0c556e1
Brian Holt <bharrisonholt@gmail.com>
2020-05-04 11:47:44 -0700

    swiping

--- components/FavoritesComponent.js
+++ components/FavoritesComponent.js
@@ -4,7 +4,12 @@ import { ListItem } from 'react-native-elements';
 import { connect } from 'react-redux';
 import { Loading } from './LoadingComponent';
 import { baseUrl } from '../shared/baseUrl';
+import Swipeout from 'react-native-swipeout';
+import {deleteFavorite} from '../redux/ActionCreators'
 
+const mapDispatchToProps= {
+    deleteFavorite: campsiteId => (deleteFavorite(campsiteId))
+}
 
 const mapStateToProps = state => {
     return {
@@ -22,14 +27,24 @@ class Favorites extends Component{
 
     render (){
         const { navigate } = this.props.navigation;
+       
         const renderFavoriteItem= ({item}) => {
+            const rightButton=[
+                {
+                    text:'Delete',
+                    type:'delete',
+                    onPress: () => this.props.deleteFavorite(item.id)
+                }
+            ]
             return(
-                <ListItem
-                    title={item.name}
-                    subtitle={item.description}
-                    leftAvatar={{source: {uri: baseUrl + item.image}}}
-                    onPress={()=> navigate('CampsiteInfo', {campsiteId: item.id})}
-               />
+                <Swipeout right={rightButton} autoClose={true}>
+                    <ListItem
+                        title={item.name}
+                        subtitle={item.description}
+                        leftAvatar={{source: {uri: baseUrl + item.image}}}
+                        onPress={()=> navigate('CampsiteInfo', {campsiteId: item.id})}
+                />
+               </Swipeout>
             )
         }
         if (this.props.campsites.isLoading){
@@ -54,5 +69,5 @@ class Favorites extends Component{
     }
 }
 
-export default connect(mapStateToProps)(Favorites);
+export default connect(mapStateToProps, mapDispatchToProps)(Favorites);
 
