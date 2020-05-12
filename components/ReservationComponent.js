e6bab2271b8a6a479c07a3ff93c1f0edfd7e0dde
Brian Holt <bharrisonholt@gmail.com>
2020-05-12 10:57:10 -0700

    async

--- components/ReservationComponent.js
+++ components/ReservationComponent.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import { Text, View, ScrollView, StyleSheet, Picker, Switch, Button, Alert } from 'react-native';
 import DatePicker from 'react-native-datepicker';
 import * as Animatable from 'react-native-animatable';
+import * as Permissions from 'expo-permissions';
+import { Notifications } from 'expo';
 
 class Reservation extends Component {
     constructor(props) {
@@ -30,6 +32,28 @@ class Reservation extends Component {
         console.log(JSON.stringify(this.state));
     }
 
+    async obtainNotificationPermission (){
+        const permission = await Permissions.getAsync(Permissions.USER_FACING_NOTIFICATIONS);
+        if (permission.status !== 'granted'){
+            const permission = await Permissions.askAsync(Permissions.USER_FACING_NOTIFICATIONS);
+            if (permission.status !== 'granted'){
+                Alert.alert('Permission not granted to show notifications')
+            }
+            return permission;
+        }
+        return permission;
+    }
+
+    async presentLocalNotifications(date){
+        const permission = await this.obtainNotificationPermission()
+        if (permission.status === 'granted'){
+            Notifications.presentLocalNotificationAsync({
+                title: 'Your Campsite Reservation Search',
+                body: 'Search for ' + date + ' requested'
+            })
+        }
+    }
+
     render() {
 
         const createTwoButtonAlert = () =>
@@ -43,7 +67,11 @@ class Reservation extends Component {
                         onPress: () => this.resetForm(),
                         style: "cancel"
                     },
-                    { text: "OK", onPress: () => this.resetForm() }
+                    { text: "OK", onPress: () => {
+                        this.resetForm();
+                        this.presentLocalNotifications(this.state.date)
+                        }
+                    }
                 ],
                 { cancelable: false }
             );
